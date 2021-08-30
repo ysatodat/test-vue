935bb1024133494e5934c44844efd0a3325216ca
Yossy <>
2021-08-30 22:52:24 +0900

    add autoprefixer and sass

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -31,10 +31,40 @@ const count = ref(0)
     Edit
     <code>components/HelloWorld.vue</code> to test hot module replacement.
   </p>
+  <div class="box" :class="{ 'box-finish': !(count < 10) }"><span v-if="count < 10" class="box-text">plz {{ 10 - count }} counts</span></div>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 a {
   color: #42b983;
+}
+
+.box {
+  width: 200px;
+  height: 200px;
+  background-color: pink;
+  margin: 80px auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: background-color ease-out 1s;
+
+  &-finish {
+    @keyframes spin {
+      from {
+        transform: rotate(0deg);
+      }
+      to {
+        transform: rotate(360deg);
+      }
+    }
+
+    animation: spin 1s infinite linear;
+    background-color: #42b983;
+  }
+
+  &-text {
+    font-weight: bold;
+  }
 }
 </style>
